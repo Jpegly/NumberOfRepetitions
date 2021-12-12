964dc9d95b0aa6ab1e657e71fa53c99b7e2b4580
Jpegly <Jpegly@JPEGLY>
2021-12-12 18:31:23 +0600

    Добавьте файлы проекта.

---
+++ NumberOfRepetitions/Algorithm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfRepetitions
+{
+    internal class Algorithm
+    {
+        private int _countMax = 0;
+        private string _symbolMax = string.Empty;
+        public Algorithm(Values values)
+        {
+            CountofSymbol(values);
+        }
+
+        public string GetCountMax
+        {
+            get
+            {
+                return _countMax.ToString();
+            }
+        }
+
+        public string GetSymbolMax
+        {
+            get
+            {
+                return _symbolMax.ToString();
+            }
+        }
+
+        private void CountofSymbol(Values values)
+        {
+            string symbol = values.ListValues[0];
+            int count = 0;
+
+            foreach (string value in values.ListValues)
+            {
+                if (symbol.Equals(value))
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count > _countMax)
+                    {
+                        _symbolMax = symbol;
+                        _countMax = count;
+                    }
+                    count = 0;
+                    symbol = value.ToString();
+                    count++;
+                }
+            }
+        }
+    }
+}
---
+++ NumberOfRepetitions/Reader.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace NumberOfRepetitions
+{
+    internal class Reader
+    {
+        private int _countFile = 4;
+
+        public Reader(Values values)
+        {
+            ReadJSON(values);
+            ReadXML(values);
+        }
+        private void ReadJSON(Values values)
+        {
+            for (int i = 1; i < _countFile; i++)
+            {
+                var obj = JObject.Parse(File.ReadAllText($"D:\\TestTask\\Files\\file_{_countFile}.json"));
+                try
+                {
+                    foreach (var element in obj)
+                    {
+                        foreach (var v in element.Value)
+                        {
+                            values.ListValues.Add(v.ToString());
+                        }
+                    }
+                }
+                catch { }
+            }
+        }
+
+        private void ReadXML(Values values)
+        {
+            for (int i = 1; i < _countFile; i++)
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load($"D:\\TestTask\\Files\\file_{_countFile}.xml");
+                try
+                {
+                    XmlElement elements = doc.DocumentElement;
+
+                    foreach (XmlElement element in elements)
+                    {
+                        foreach (XmlNode childnode in element.ChildNodes)
+                        {
+                            if (childnode.Name == "Value")
+                            {
+                                values.ListValues.Add(childnode.InnerText);
+                            }
+                        }
+                    }
+                }catch { }
+            }
+        }
+    }
+}
